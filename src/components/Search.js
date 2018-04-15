a41c883917d0bc16010d33552c6b8ca0753c5bae
liannaccone <lciannaccone@gmail.com>
2018-04-15 15:08:15 -0700

    removing unused files, cleaning up code, and adding directions in Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,11 @@ const Search = props => (
 			<h3 className='panel-title'>Search</h3>
 		</div>
 		<div className='panel-body'>
+			<br />
+			<h6>Enter a medication in the search bar below. </h6>
+			<br />
+			<h6>Results will generate to the right where you can then select the appropriate dosage to find a comparable alternative </h6>
+			<br />
 			<form>
 			    <div className="form-group">
 			    	<input
